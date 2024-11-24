b8447eff12a5f61c444c4ce5a79e888dd126a8d4
vishnubuvan <vishnubuvan2897@gmail.com>
2024-11-23 22:30:35 -0800

    push my branch

--- src/test/java/org/getname/PrintYourName.java
+++ src/test/java/org/getname/PrintYourName.java
@@ -1,5 +1,14 @@
 package org.getname;
 
 public class PrintYourName {
+	
+public void name() {
+	System.out.println("my name is vishnudarshan");
+	}
 
+	public static void main(String[] args) {
+		PrintYourName a = new PrintYourName();
+		a.name();
+	}
 }
+
