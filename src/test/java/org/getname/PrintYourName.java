df851726730da7b70e1ff6b2e17f5312b00620fd
Ramkarthikselva <ramkarthikselva@gmail.com>
2024-11-24 12:45:14 +0530

    to branch update

--- src/test/java/org/getname/PrintYourName.java
+++ src/test/java/org/getname/PrintYourName.java
@@ -1,14 +1,25 @@
 package org.getname;
 
 public class PrintYourName {
-	public static void main(String[] args) {
+	
 		
+		public void name() {
+			System.out.println("KARTHIK");
+		}
+		public void name1() {
+			System.out.println("MohammedSuhail");
+		}
 		
-		System.out.println("KARTHIK");
-}
-
-	public void name() {
-		System.out.println("MohammedSuhail");
+		public void id() {
+			System.out.println("1234");
+		}
+	
+		public static void main(String[] args) {
+			PrintYourName p= new PrintYourName();
+			p.name();
+			p.name1();
+			p.id();
 		}
 	}
+		
 		
